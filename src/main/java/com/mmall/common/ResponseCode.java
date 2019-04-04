7c10f4fd5813bcc1622494acaaa4c4c6834e7b45
qm1083824922 <1083824922@qq.com>
2019-04-04 14:38:06 +0800

    project initial

---
+++ src/main/java/com/mmall/common/ResponseCode.java
@@ -0,0 +1,32 @@
+package com.mmall.common;
+
+/**
+ * Created by geely
+ * @author Administrator
+ */
+public enum ResponseCode {
+    /**
+     *返回状态码
+     */
+    SUCCESS(0,"SUCCESS"),
+    ERROR(1,"ERROR"),
+    NEED_LOGIN(10,"NEED_LOGIN"),
+    ILLEGAL_ARGUMENT(2,"ILLEGAL_ARGUMENT");
+
+    private final int code;
+    private final String desc;
+
+
+    ResponseCode(int code,String desc){
+        this.code = code;
+        this.desc = desc;
+    }
+
+    public int getCode(){
+        return code;
+    }
+    public String getDesc(){
+        return desc;
+    }
+
+}
